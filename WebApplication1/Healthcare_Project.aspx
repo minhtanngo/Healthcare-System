0068d59a33a182ba6944b3359ef60910da6da4b0
Minh Tan Ngo <74533550+minhtanngo@users.noreply.github.com>
2020-12-14 21:39:04 +0700

    Add files via upload

---
+++ WebApplication1/Healthcare_Project.aspx
@@ -0,0 +1,18 @@
+﻿
+
+<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Healthcare_Project.aspx.cs" Inherits="WebApplication1.Healthcare_Project" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <asp:GridView ID="GridView1" runat="server"></asp:GridView>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebApplication1/Healthcare_Project.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using System.Data;
+using System.Data.SqlClient;
+
+
+namespace WebApplication1
+{
+    public partial class Healthcare_Project : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //Khai bao ket noi toi MS SQL Server
+            SqlConnection con = new SqlConnection("server=DESKTOP-SG1DHTO\\SQLEXPRESS;database=HeathcareProject;Integrated security = true;");
+
+            //Thuc thi cau query
+            SqlDataAdapter da = new SqlDataAdapter("select * from BENHNHAN", con);
+
+            //Tao mot bảng ảo
+            DataTable tb = new DataTable();
+            da.Fill(tb);
+
+            //Ném data vào gridview
+            GridView1.DataSource = tb;
+            GridView1.DataBind();
+        }
+    }
+}
